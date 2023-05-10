3fed9c307e292a5580690173b7217fec343552d7
Wescley Martins Sardinha Teixeira <wescleymartins@brq.com>
2023-05-10 14:24:37 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -23,15 +23,17 @@ public class Game {
 	private String genre;
 	private String platforms;
 	private Double score;
-	private String imagUrl;
+	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imagUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
@@ -39,7 +41,7 @@ public class Game {
 		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
-		this.imagUrl = imagUrl;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -92,12 +94,12 @@ public class Game {
 		this.score = score;
 	}
 
-	public String getImagUrl() {
-		return imagUrl;
+	public String getImgUrl() {
+		return imgUrl;
 	}
 
-	public void setImagUrl(String imagUrl) {
-		this.imagUrl = imagUrl;
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
